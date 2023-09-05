fe9ade8b56daf8350ddaeed643da65fa2e02fdf2
subhadeephazra <subhadeephazra68@gmail.com>
2023-09-05 20:26:13 +0530

    first commit

--- script.js
+++ script.js
@@ -37,16 +37,16 @@ window.addEventListener("load", async function fetchSearchResults(searchString) 
   })
   function navigateToVideoDetails(videoId, videoobject) {
     localStorage.setItem("videoObject", JSON.stringify(videoobject));
-  // document.cookie = `id=${videoId}; path=https://subhadeephazra.github.io/youtube_clone/play-video.html`;
+  // document.cookie = `id=${videoId}; path=https://subhadeephazra.github.io/Youtube_Clone/play-video.html`;
   localStorage.setItem("id", videoId);
-  window.location.href = "https://subhadeephazra.github.io/youtube_clone/play-video.html";
+  window.location.href = "https://subhadeephazra.github.io/Youtube_Clone/play-video.html";
 }
 function navigatetoChannelPage(videochannelid) {
   localStorage.setItem("channelid", videochannelid);
-  window.location.href="https://subhadeephazra.github.io/youtube_clone/channel.html"
+  window.location.href="https://subhadeephazra.github.io/Youtube_Clone/channel.html"
 }
 // function navigateToVideotitles(Content) {
-//   document.cookie = `title=${Content}; path=/youtube-clone/play-video.html`;
+//   document.cookie = `title=${Content}; path=/Youtube_clone/play-video.html`;
 // }
   function calculateTheTimeGap(publishTime) {
     let publishDate = new Date(publishTime);
